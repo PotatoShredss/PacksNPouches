2d48e6313a904fe92fddfec4e1a26380d5f0ab8b
winson lu <Winson3409@gmail.com>
2025-06-28 00:13:30 -0400

    thca-0.3

--- src/main/java/com/potatoshreds/thca/renderer/CurioRegistry.java
+++ src/main/java/com/potatoshreds/thca/renderer/CurioRegistry.java
@@ -6,10 +6,21 @@ import top.theillusivec4.curios.api.client.CuriosRendererRegistry;
 public class CurioRegistry {
 
     public static void registerCurios(){
-        CuriosRendererRegistry.register(THCAItems.HIKINGPACK.get(), PackRenderer::new);
-        CuriosRendererRegistry.register(THCAItems.KNAPSACK.get(), PackRenderer::new);
         CuriosRendererRegistry.register(THCAItems.SMALLPOUCH.get(), PouchRenderer::new);
+        CuriosRendererRegistry.register(THCAItems.LARGEPOUCH.get(), PouchRenderer::new);
         CuriosRendererRegistry.register(THCAItems.TOOLBELT.get(), BeltRenderer::new);
         CuriosRendererRegistry.register(THCAItems.SPECOPSUTILITYBELT.get(), BeltRenderer::new);
+
+        CuriosRendererRegistry.register(THCAItems.HIKINGPACK.get(), PackRenderer::new);
+        CuriosRendererRegistry.register(THCAItems.KNAPSACK.get(), PackRenderer::new);
+        CuriosRendererRegistry.register(THCAItems.DAYBAG.get(), PackRenderer::new);
+        CuriosRendererRegistry.register(THCAItems.BOOKBAG.get(), PackRenderer::new);
+        CuriosRendererRegistry.register(THCAItems.SPECOPSBAG.get(), PackRenderer::new);
+        CuriosRendererRegistry.register(THCAItems.TOOLBOX.get(), ToolBoxPackRenderer::new);
+        CuriosRendererRegistry.register(THCAItems.DRAWSTRINGBAG.get(), PackRenderer::new);
+        CuriosRendererRegistry.register(THCAItems.CLOTHBAG.get(), PackRenderer::new);
+        CuriosRendererRegistry.register(THCAItems.HANDMADEKNAPSACK.get(), PackRenderer::new);
+        CuriosRendererRegistry.register(THCAItems.LEATHERBAG.get(), PackRenderer::new);
+        CuriosRendererRegistry.register(THCAItems.MILITARYBAG.get(), PackRenderer::new);
     }
 }
---
+++ src/main/java/com/potatoshreds/thca/renderer/ToolBoxPackRenderer.java
@@ -0,0 +1,45 @@
+package com.potatoshreds.thca.renderer;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.RenderLayerParent;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
+import top.theillusivec4.curios.api.SlotContext;
+import top.theillusivec4.curios.api.client.ICurioRenderer;
+
+public class ToolBoxPackRenderer implements ICurioRenderer{
+    @Override
+    public <T extends LivingEntity, M extends EntityModel<T>> void render(
+            ItemStack itemStack,
+            SlotContext slotContext,
+            PoseStack poseStack,
+            RenderLayerParent<T, M> renderLayerParent,
+            MultiBufferSource multiBufferSource,
+            int lightV,
+            float limbSwing,
+            float limbSwingAmount,
+            float partialTicks,
+            float ageInTicks,
+            float netHeadYaw,
+            float headPitch ){
+        LivingEntity entity = slotContext.entity();
+        if(!itemStack.isEmpty()){
+            poseStack.pushPose();
+            ICurioRenderer.translateIfSneaking(poseStack, entity);
+            ICurioRenderer.rotateIfSneaking(poseStack, entity);
+            poseStack.scale(1.5F,1.5F,1.5F);
+            poseStack.mulPose(Axis.ZP.rotationDegrees(150));
+            poseStack.translate(0.09,0.525,0.58);
+            Minecraft.getInstance().getItemRenderer().renderStatic(
+                    itemStack, ItemDisplayContext.HEAD, lightV, OverlayTexture.NO_OVERLAY, poseStack, multiBufferSource, entity.level(), 0
+            );
+            poseStack.popPose();
+        }
+    }
+}
